cbe001f273908d65faa3baaa2c592774b42623b0
me@seif.codes <me@seif.codes>
2018-08-11 09:18:06 +0000

    add example

---
+++ example/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/seiflotfy/quantiles"
+)
+
+func main() {
+	qstream, err := quantiles.New(0.01, 1<<4)
+	if err != nil {
+		panic(err)
+	}
+	for i := 0.0; i < 1e6; i++ {
+		if err := qstream.Push(i, 1.0); err != nil {
+			panic(err)
+		}
+	}
+	fmt.Println(qstream.ApproximationError(1))
+	fmt.Println(qstream.Finalize())
+	fmt.Println(qstream.GenerateQuantiles(4))
+	fmt.Println(qstream.GenerateQuantiles(10))
+	fmt.Println(qstream.GenerateQuantiles(4))
+	sum, err := qstream.FinalSummary()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(sum.GenerateQuantiles(4))
+}
