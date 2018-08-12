76923dd6cf1d91cd0c6d03e0e925249be5674133
me@seif.codes <me@seif.codes>
2018-08-12 21:20:16 +0000

    move things around again

--- summary.go
+++ summary.go
@@ -1,5 +1,21 @@
 package quantiles
 
+// SumEntry ...
+type SumEntry struct {
+	Value   float64
+	Weight  float64
+	MinRank float64
+	MaxRank float64
+}
+
+func (se SumEntry) prevMaxRank() float64 {
+	return se.MaxRank - se.Weight
+}
+
+func (se SumEntry) nextMinRank() float64 {
+	return se.MinRank + se.Weight
+}
+
 // Summary ...
 type Summary struct {
 	entries []*SumEntry
